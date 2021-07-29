511fa3b391a3dff1c0e30b70d596b131c6d11dce
Krzysztof Jakubowski <80991593+Kriss-profi@users.noreply.github.com>
2021-07-29 13:05:58 +0200

    Dodaj pliki projektów.

---
+++ Kasjer/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Kasjer
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private const string S = "Siema";
+        private const string P = "Przelicz";
+        public MainWindow()
+        {
+            InitializeComponent();
+            Suma.Text = "0.00";
+        }
+
+        private void Przelicz_Click(object sender, RoutedEventArgs e)
+        {
+            Przelicz.Content = S;
+        }
+
+        private void Drukuj_Click(object sender, RoutedEventArgs e)
+        {
+            Przelicz.Content = P;
+        }
+    }
+}
